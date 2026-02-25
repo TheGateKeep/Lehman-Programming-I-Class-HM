92a97d48aec0d640623e3cef9c5d2fff16610a85
TheGateKeep <92342324+TheGateKeep@users.noreply.github.com>
2026-02-25 17:25:47 -0500

    Update Words.java

--- 2/Words.java
+++ 2/Words.java
@@ -8,7 +8,7 @@ public class Words {
     String in = scn.nextLine();
     
     boolean t = true;
-    int[] wordBounds = new int[9];  //just ignore the 9th value
+    int[] wordBounds = new int[8]; 
     wordBounds[0] = 0;              //sets first start to 0
     wordBounds[7] = in.length()-1;  //sets last to end of string
     /*
@@ -18,7 +18,7 @@ public class Words {
     */
     for (int j = -1; j < 3; j++) {   //j goes from 0 - 4, being each word
         for (int i = 0; t == false; i++) {  //i goes through each letter in the word to find the end
-            if (in.charAt(i+wordBounds[j*2]) == ' ') { //9th value prevents this from going out of bounds
+            if (in.charAt(i+wordBounds[j*2]) == ' ') {
                 t = true;
                 wordBounds[(2*j)+1] = i+wordBounds[j*2] - 1;
                 wordBounds[(2*j)+2] = i+wordBounds[j*2] + 1;
@@ -26,8 +26,52 @@ public class Words {
         }
         t = false;
     }
-    for (int i = 0; i < 8; i++) {
-        System.out.println(wordBounds[i]);
+    
+    int[][] vowels = {{-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1}};
+    boolean[] vowelFound = {false,false,false,false,false};
+    /*
+        int[a][b]
+        a is which word is being looked at, 0-3
+        b is which vowel, order a e i o u for 0-4
+    */
+    
+    for (int k = 0; k < 4; k++) {                                   //k is each word
+        for (int m = wordBounds[k]; m <= wordBounds[k+1]; m++) {    //m indexes through each letter
+            switch (in.charAt(m)) {
+                case 'a':
+                    if (vowelFound[0] == false) {
+                        vowels[k][0] = m - wordBounds[k];
+                        vowelFound[0] = true;
+                    }
+                    break;
+                case 'e':
+                    if (vowelFound[1] == false) {
+                        vowels[k][1] = m - wordBounds[k];
+                        vowelFound[1] = true;
+                    }
+                    break;
+                case 'i':
+                    if (vowelFound[2] == false) {
+                        vowels[k][2] = m - wordBounds[k];
+                        vowelFound[2] = true;
+                    }
+                    break;
+                case 'o':
+                    if (vowelFound[3] == false) {
+                        vowels[k][3] = m - wordBounds[k];
+                        vowelFound[3] = true;
+                    }
+                    break;
+                case 'u':
+                    if (vowelFound[4] == false) {
+                        vowels[k][4] = m - wordBounds[k];
+                        vowelFound[4] = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
 }
