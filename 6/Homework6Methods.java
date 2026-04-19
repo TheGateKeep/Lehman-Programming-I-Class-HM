e43bb93f1bddf40f16035391b133f9a94140d9b6
TheGateKeep <92342324+TheGateKeep@users.noreply.github.com>
2026-04-19 17:17:03 -0400

    Update Homework6Methods.java

--- 6/Homework6Methods.java
+++ 6/Homework6Methods.java
@@ -2,10 +2,62 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Homework6Methods {
-  Scanner scn = new Scanner(System.in);
+  static Scanner scn = new Scanner(System.in);
   
   public static void main(String[] args) {
+    //input system used for testing
+    System.out.print("Select Method: ");
+    int p = scn.nextInt();
+    System.out.println("Grant Input: ");
     
+    switch (p) {
+        case 0:
+            System.out.println(getMaxOf2Ints(scn.nextInt(),scn.nextInt()));
+            break;
+        case 1:
+            System.out.println(getMinOf2Ints(scn.nextInt(),scn.nextInt()));
+            break;
+        case 2:
+            System.out.println(getMaxOf3Ints(scn.nextInt(),scn.nextInt(),scn.nextInt()));
+            break;
+        case 3:
+            System.out.println(getMedianOf3Ints(scn.nextInt(),scn.nextInt(),scn.nextInt()));
+            break;
+        case 4:
+            PrintMinOf3Ints(scn.nextInt(),scn.nextInt(),scn.nextInt());
+            break;
+        case 5:
+            System.out.println(getProdOfAllPositiveInts(scn.nextInt()));
+            break;
+        case 6:
+            System.out.println(getProdOfAllNegativeInts(scn.nextInt()));
+            break;
+        case 7:
+            System.out.println(isProdOfAllNegativeIntsNegative(scn.nextInt()));
+            break;
+        case 8:
+            String fix1 = scn.nextLine();
+            //was scanning for the the next line before input, this is here to skip that line
+            System.out.println(getCharAtIndex(scn.nextLine(),scn.nextInt()));
+            break;
+        case 9:
+            String fix2 = scn.nextLine();
+            //ditto
+            System.out.println(getCountOfCharInString(scn.nextLine(),scn.next().charAt(0)));
+            break;
+        case 10:
+            String fix3 = scn.nextLine();
+            //ditto
+            System.out.println(getStringReversed(scn.nextLine()));
+            break;
+        case 11:
+            String fix4 = scn.nextLine();
+            //ditto
+            System.out.println(getStringTitleCased(scn.nextLine()));
+            break;
+        default:
+            System.out.println("Error: Beyond Method List");
+    }
   }
   
   public static int getMaxOf2Ints(int a, int b) {
@@ -36,7 +88,7 @@ public class Homework6Methods {
   }
   
   public static void PrintMinOf3Ints(int a, int b, int c) {
-    System.out.print("The min is "+getMinOf2Ints(getMinOf2Ints(a,b),c))
+    System.out.print("The min is "+getMinOf2Ints(getMinOf2Ints(a,b),c));
   }
   
   public static int getProdOfAllPositiveInts(int a) {
@@ -51,14 +103,14 @@ public class Homework6Methods {
   }
   
   public static int getProdOfAllNegativeInts(int a) {
-    if (a >= 0) {return 0;}
-    else {
+    if (a < 0) {
         int out = 1;
-        for (int i = 1; i < a; i--) {
+        for (int i = -2; i > a; i--) {
             out *= i;
         }
         return out;
     }
+    else {return 0;}
   }
   
   public static boolean isProdOfAllNegativeIntsNegative(int a) {
@@ -74,29 +126,39 @@ public class Homework6Methods {
   }
   
   public static int getCountOfCharInString(String s, char c) {
-    int out;
+    int out = 0;
     for (int i = 0; i < s.length(); i++) {
-        if (s.charAt(i)==c) {out++;}
+        if (s.charAt(i) == c) {out++;}
     }
     return out;
   }
   
-  public static int getStringReversed(String s) {
+  public static String getStringReversed(String s) {
     char[] a = new char[s.length()];
     Arrays.fill(a,' ');
     
-    for (int i = s.length(); i > 0; i--) {
-        
-        //continue
+    for (int i = s.length()-1; i >= 0; i--) {
+        a[s.length()-i-1] = s.charAt(i);
     }
     
-    return new String(array);
+    return new String(a);
   }
   
-  public static int getStringTitleCased(String s) {
-    if (s == null || s == "") {return "";}
+  public static String getStringTitleCased(String s) {
+    if (s == null || s == "" || s.length() == 0) {return "";}
     else {
-        //continue
+        char[] a = new char[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            a[i] = s.charAt(i);
+            a[i] = Character.toLowerCase(a[i]);
+            if (a[i-1] == ' ') {
+                a[i] = Character.toUpperCase(a[i]);
+            }
+        }
+        a[0] = s.charAt(0);
+        a[0] = Character.toUpperCase(a[0]);
+        
+        return new String(a);
     }
   }
 }
